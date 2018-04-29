75202f39642ef485cb7fea13c818514a0ccd4a74
dragosbus <dragosbusuioc94@gmail.com>
2018-04-29 11:41:47 +0200

    create new state data when a button is clicked, create new random nubmer when a button is clicked and change color of a element when a button is clicked

--- lecture/src/App.js
+++ lecture/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import Main from './components/Main';
+import ReactDOM from 'react-dom';
 import './App.css';
 
 
@@ -11,6 +10,69 @@ class App extends Component {
         <Header />
         <Main/>
       </div>
+    );
+  }
+}
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      headerText: "Welcome to React"
+    }
+  }
+  render() {
+    return (
+      <header className="main-header">
+        <h1>{this.state.headerText}</h1>
+      </header>
+    );
+  }
+}
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      count: 0
+    }
+    this.updateState = this.updateState.bind(this);
+    this.forceUpdateRandom = this.forceUpdateRandom.bind(this);
+    this.findMyDOMNode = this.findMyDOMNode.bind(this);
+  }
+
+  updateState() {
+    let item = `Click-${this.state.count}`;
+    let arr = [];
+    arr.push(item);
+    this.setState({
+      data: arr,
+      count: this.state.count + 1
+    });
+  }
+
+  forceUpdateRandom() {
+    this.forceUpdate();
+  }
+
+  findMyDOMNode() {
+    let myHeading = document.querySelector('.heading4');
+    ReactDOM.findDOMNode(myHeading).style.color = 'red';
+  }
+  
+  render() {
+    return (
+      <main>
+        <h2>ReactJS for beginners</h2>
+        <h3>Components API</h3>
+        <button onClick={this.updateState}>Click</button>
+        <h4>State Data: {this.state.data}</h4>
+        <button onClick={this.forceUpdateRandom}>Random Number</button>
+        <h4>Random Number: {Math.random()}</h4>
+        <button onClick={this.findMyDOMNode}>FidDOM</button>
+        <h4 className="heading4">My heading</h4>
+      </main>
     );
   }
 }
