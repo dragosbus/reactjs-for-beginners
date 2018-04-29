6a53948af644a6e50eeb79f71685fead726a9b1f
dragosbus <dragosbusuioc94@gmail.com>
2018-04-29 12:13:39 +0200

    crated a number component and update the number by 1 when a button is clicked

--- lecture/src/App.js
+++ lecture/src/App.js
@@ -35,11 +35,13 @@ class Main extends Component {
     super(props);
     this.state = {
       data: [],
-      count: 0
+      count: 0,
+      number: 0
     }
     this.updateState = this.updateState.bind(this);
     this.forceUpdateRandom = this.forceUpdateRandom.bind(this);
     this.findMyDOMNode = this.findMyDOMNode.bind(this);
+    this.setNewNumber = this.setNewNumber.bind(this);
   }
 
   updateState() {
@@ -60,7 +62,13 @@ class Main extends Component {
     let myHeading = document.querySelector('.heading4');
     ReactDOM.findDOMNode(myHeading).style.color = 'red';
   }
-  
+
+  setNewNumber() {
+    this.setState({
+      number: this.state.number + 1
+    });
+  }
+
   render() {
     return (
       <main>
@@ -72,7 +80,28 @@ class Main extends Component {
         <h4>Random Number: {Math.random()}</h4>
         <button onClick={this.findMyDOMNode}>FidDOM</button>
         <h4 className="heading4">My heading</h4>
+        <NumberComponent number={this.state.number} setNewNumber={this.setNewNumber}/>
       </main>
+    );
+  }
+}
+
+class NumberComponent extends Component {
+  componentWillMount() {
+    console.log('Compo will mount')
+  }
+  componentDidMount() {
+    console.log('COmpo did mount')
+  }
+  componentWillReceiveProps() {
+    console.log('Will receive props');
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={()=>this.props.setNewNumber()}>Update Number</button>
+        <h4>{this.props.number}</h4>
+      </div>
     );
   }
 }
