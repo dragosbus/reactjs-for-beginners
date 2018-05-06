f21ee085005ffee469d1d8ad0e7782ecb532f6cb
dragosbus <dragosbusuioc94@gmail.com>
2018-05-06 12:03:38 +0200

    created header component

---
+++ game/src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export class Header extends Component {
+    render() {
+        return (
+            <header className="main-header">
+                <h1>Quiz Game</h1>
+            </header>
+        );
+    }
+}
--- game/src/components/Quiz.js
+++ game/src/components/Quiz.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import './App.css';
 
 
 export class Quiz extends Component {
     render() {
         return (
-            
+            <div className="quiz">
+                <div className="quiz-content">
+                    <p className="question">
+                        What is the sum of 5+5?
+                    </p>
+                    <div className="options">
+                        <div className="fields">10</div>
+                        <div className="fields">20</div>
+                        <div className="fields">30</div>
+                        <div className="fields">40</div>
+                    </div>
+                </div>
+                <div className="play-again">
+                    <a className="button">Play Again</a>
+                </div>
+            </div>
         );
     }
 }
