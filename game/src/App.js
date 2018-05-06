43ba2ef93b11a64eca359ebc188d5bb09b439d6d
dragosbus <dragosbusuioc94@gmail.com>
2018-05-06 12:16:48 +0200

    style the page

--- game/src/App.js
+++ game/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import {Quiz} from './components/Quiz';
+import { Header } from './components/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Quiz />
+      <div className="app">
+        <Header />
+        <Quiz/>
+      </div>
     );
   }
 }
